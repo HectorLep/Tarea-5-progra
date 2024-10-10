a401b14d1945184a0f6e758456471d70c7775c9e
sonickiller39 <sonickiller39@gmail.com>
2024-10-09 23:11:07 -0300

    si2.0

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from ventana_principal import Main
+import customtkinter as ctk
+from Ventanas.ventana_principal import SistemaGestionUniversitaria
 
-app = Main()
-app.resizable(False, False)
-app.mainloop()  
+if __name__ == "__main__":
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
+
+    app = SistemaGestionUniversitaria()
+    app.mainloop()
